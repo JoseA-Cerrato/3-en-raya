88f1b83bafb363111cdc5e4284835a5a37a6c26d
Jose Antonio <joseantonio.cerrato98@gmail.com>
2025-03-02 13:28:26 +0100

    [Add ] #1 Insertada la comprobación de casilla

--- 3-en-raya/src/App.jsx
+++ 3-en-raya/src/App.jsx
@@ -7,40 +7,49 @@ import './App.css'
 function App() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [isTurn_X, setTurn] = useState(true);
+  const [message, setMessage] = useState('')
   function clickSquare(i) {
     const nextSquares = history.slice();
-    if (isTurn_X ){
-      nextSquares[i] = "X";
+    if (nextSquares[i] !== undefined){
+      setMessage("ERROR: Casilla ya seleccionada. (" + nextSquares[i] + ")")
     }else{
-      nextSquares[i] = "O";
+      if (isTurn_X ){ 
+        nextSquares[i] = "X";
+      }else{
+        nextSquares[i] = "O";
+      }
+
+      //TODO --> Comprobaciones de victoria
+      setHistory(nextSquares);
+      setTurn(!isTurn_X)
     }
-
-    //TODO --> Comprobaciones de victoria
-    setHistory(nextSquares);
-    setTurn(!isTurn_X)
-
 
   }
   return (
     <>
     <div className="board-row" >
 
-      <Square value={history[0]} onClickedSquare={() => clickSquare(0)}></Square>
       <Square value={history[1]} onClickedSquare={() => clickSquare(1)}></Square>
       <Square value={history[2]} onClickedSquare={() => clickSquare(2)}></Square>
+      <Square value={history[3]} onClickedSquare={() => clickSquare(3)}></Square>
       </div>
       <div className="board-row" >
-      <Square value={history[3]} onClickedSquare={() => clickSquare(3)}></Square>
       <Square value={history[4]} onClickedSquare={() => clickSquare(4)}></Square>
       <Square value={history[5]} onClickedSquare={() => clickSquare(5)}></Square>
+      <Square value={history[6]} onClickedSquare={() => clickSquare(6)}></Square>
       </div>
       <div className="board-row" >
-      <Square value={history[6]} onClickedSquare={() => clickSquare(6)}></Square>
       <Square value={history[7]} onClickedSquare={() => clickSquare(7)}></Square>
       <Square value={history[8]} onClickedSquare={() => clickSquare(8)}></Square>
+      <Square value={history[9]} onClickedSquare={() => clickSquare(9)}></Square>
     </div>
 
+    <div className="Message-text">
+      {message}
+    </div>
     </>
+
+
   )
 }
 
